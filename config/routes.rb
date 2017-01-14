e89213110988ba47b14f4879f1af51e5b5a74c0f
vikasvking <vikas.vk711@gmail.com>
2017-01-15 00:45:11 +0530

    edited some styel

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users
+  resources :posts
   root "posts#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
