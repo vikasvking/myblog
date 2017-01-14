2d52f1451733ae6b1c99c241308c4156f8083b52
vikasvking <vikas.vk711@gmail.com>
2017-01-15 01:44:23 +0530

    some styling

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def index
-    @post=Post.all.order('created_at DESC').paginate(:page=>params[:page],:per_page=>6)
+    @post=Post.all.order('created_at DESC').paginate(:page=>params[:page],:per_page=>2)
   end
 
   def show
